e964b6f4ca8526cf2d5e875558dd905ff978ce2e
tholkappiar <tholkappiar2003@gmail.com>
2024-03-20 21:21:47 +0530

    Day 6.
    Spring Boot Setup

---
+++ edugateway/src/main/java/com/backend/edugateway/Entity/Signup.java
@@ -0,0 +1,18 @@
+package com.backend.edugateway.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Signup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}

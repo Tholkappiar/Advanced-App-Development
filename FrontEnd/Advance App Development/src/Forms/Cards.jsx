6009e4de63731b476807f3f56f225fae5e2c7530
tholkappiar <tholkappiar2003@gmail.com>
2024-03-31 20:14:05 +0530

    feat: Frontend Card-Input

--- FrontEnd/Advance App Development/src/Forms/Cards.jsx
+++ FrontEnd/Advance App Development/src/Forms/Cards.jsx
@@ -40,32 +40,40 @@ const data = [
 
 const Cards = () => {
 	return (
-		<div className="flex justify-around bg-[#F9F5EB] flex-wrap h-[100%] py-10">
-			{data.map((college) => (
-				<div
-					key={college.name}
-					className="flex bg-gray-300 rounded-lg items-center shadow-lg m-4 min-w-[295px] mobileS:w-[455px] flex-col mobileS:flex-row h-[300px]"
-				>
-					<div className="flex flex-col justify-between items-center m-4 w-6/12">
-						<img
-							src="https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29sbGVnZXxlbnwwfHwwfHx8MA%3D%3D"
-							className="rounded-lg object-cover  w-11/12"
-						/>
-						<Link
-							to={"/form"}
-							className="bg-[#EA5455] rounded-lg m-2 p-3 font-semibold w-20"
-						>
-							Enroll
-						</Link>
+		<div className="bg-[#F9F5EB]">
+			<div className="flex justify-center">
+				<input
+					className="m-4 rounded-lg shadow-md p-2 outline-none"
+					placeholder="search"
+				></input>
+			</div>
+			<div className="flex justify-around  flex-wrap h-[100%] py-10">
+				{data.map((college) => (
+					<div
+						key={college.name}
+						className="flex bg-gray-300 rounded-lg items-center shadow-lg m-4 min-w-[295px] mobileS:w-[455px] flex-col mobileS:flex-row h-[300px]"
+					>
+						<div className="flex flex-col justify-between items-center m-4 w-6/12">
+							<img
+								src="https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29sbGVnZXxlbnwwfHwwfHx8MA%3D%3D"
+								className="rounded-lg object-cover  w-11/12"
+							/>
+							<Link
+								to={"/form"}
+								className="bg-[#EA5455] rounded-lg m-2 p-3 font-semibold w-20"
+							>
+								Enroll
+							</Link>
+						</div>
+						<div className="text-sm font-semibold text-center mobileS:text-start m-4 mt-0 mobileS:mt-4 flex flex-col w-6/12">
+							<p className="mb-2">{college.name}</p>
+							<p className="mb-2">{college.location}</p>
+							<p className="mb-2">{college.Number}</p>
+							<p className="mb-2">{college.Email}</p>
+						</div>
 					</div>
-					<div className="text-sm font-semibold text-center mobileS:text-start m-4 mt-0 mobileS:mt-4 flex flex-col w-6/12">
-						<p className="mb-2">{college.name}</p>
-						<p className="mb-2">{college.location}</p>
-						<p className="mb-2">{college.Number}</p>
-						<p className="mb-2">{college.Email}</p>
-					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	);
 };
